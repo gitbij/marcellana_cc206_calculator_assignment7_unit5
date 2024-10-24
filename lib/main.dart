5f6993311e1513262a0a0d34c819a460179d6b90
gitbij <bjlouis.marcellana@wvsu.edu.ph>
2024-10-24 17:49:31 +0800

    submission commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,28 @@ class CalculatorScreen extends StatefulWidget {
 class _CalculatorScreenState extends State<CalculatorScreen> {
   // manage the sum section
   int sum = 0;
+  int difference = 0;
+  int product = 0;
+  int quotient = 0;
   String operation = " ";
   // either use a TextEditingController for each input field to get the value
   TextEditingController add1Controller = TextEditingController();
   TextEditingController add2Controller = TextEditingController();
+  TextEditingController minus1Controller = TextEditingController();
+  TextEditingController minus2Controller = TextEditingController();
+  TextEditingController multiply1Controller = TextEditingController();
+  TextEditingController multiply2Controller = TextEditingController();
+  TextEditingController divide1Controller = TextEditingController();
+  TextEditingController divide2Controller = TextEditingController();
   // or store each value in the state
   int firstAddNum = 0;
   int secondAddNum = 0;
+  int firstMinusNum = 0;
+  int secondMinusNum = 0;
+  int firstMultiplyNum = 0;
+  int secondMultiplyNum = 0;
+  int firstDivideNum = 0;
+  int secondDivideNum = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,50 +124,178 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
 
           // 3.c - Add the other operations
           // Minus Row
-          Row (
-          children: [
-            IconButton.outlined(
-                
-                icon: Icon(Icons.remove),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: minus1Controller,
+                  decoration: InputDecoration(labelText: "First Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      firstMinusNum = int.parse(value);
+                    });
+                  },
+                ),
+              ),
+             
+              Text(operation),
+              Expanded(
+                child: TextField(
+                  controller: minus2Controller,
+                  decoration: InputDecoration(labelText: "Second Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      secondMinusNum = int.parse(value);
+                    });
+                  },
+                ),
+              ),
+
+              Text(' = $difference   '),
+              
+              IconButton.outlined(
+                icon: Icon(Icons.add),
                 onPressed: () {
                     setState(() {
-                      sum = firstAddNum - secondAddNum;
+                      difference = firstMinusNum - secondMinusNum;
                       operation = " - ";
                     });
                 } 
-              ),            
-          ] 
-          ),
-          // Multiplication Row
-          Row (
-          children: [
-            IconButton.outlined(
-                
-                icon: Icon(Icons.clear),
+              ),
+
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 15),
+                ),
                 onPressed: () {
                     setState(() {
-                      sum = firstAddNum * secondAddNum;
+                      minus1Controller.clear();
+                      minus2Controller.clear();
+                      difference = 0;
+                    });
+                }, 
+                child: Text("Clear Contents"),
+              ),              
+            ],
+          ),
+
+          // Multiplication Row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: multiply1Controller,
+                  decoration: InputDecoration(labelText: "First Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      firstMultiplyNum = int.parse(value);
+                    });
+                  },
+                ),
+              ),
+             
+              Text(operation),
+              Expanded(
+                child: TextField(
+                  controller: multiply2Controller,
+                  decoration: InputDecoration(labelText: "Second Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      secondMultiplyNum = int.parse(value);
+                    });
+                  },
+                ),
+              ),
+
+              Text(' = $product   '),
+              
+              IconButton.outlined(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                    setState(() {
+                      product = firstMultiplyNum * secondMultiplyNum;
                       operation = " * ";
                     });
                 } 
-              ),            
-          ] 
-          ),
-          // Division Row
-           Row (
-          children: [
-            IconButton.outlined(
-                //sir yawan ko ngita divide na icon :'>
-                icon: Icon(Icons.safety_divider),
+              ),
+
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 15),
+                ),
                 onPressed: () {
                     setState(() {
-                      sum = firstAddNum ~/ secondAddNum;
+                      multiply1Controller.clear();
+                      multiply2Controller.clear();
+                      product = 0;
+                    });
+                }, 
+                child: Text("Clear Contents"),
+              ),              
+            ],
+          ),
+
+          // Division Row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: divide1Controller,
+                  decoration: InputDecoration(labelText: "First Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      firstDivideNum = int.parse(value);
+                    });
+                  },
+                ),
+              ),
+             
+              Text(operation),
+              Expanded(
+                child: TextField(
+                  controller: divide2Controller,
+                  decoration: InputDecoration(labelText: "Second Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      secondDivideNum = int.parse(value);
+                    });
+                  },
+                ),
+              ),
+
+              Text(' = $quotient   '),
+              
+          
+              IconButton.outlined(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                    setState(() {
+                      quotient = firstDivideNum ~/ secondDivideNum;
                       operation = " / ";
                     });
                 } 
-              ),            
-          ] 
+              ),
+
+        
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 15),
+                ),
+                onPressed: () {
+                    setState(() {
+                      divide1Controller.clear();
+                      divide2Controller.clear();
+                      quotient = 0;
+                    });
+                }, 
+                child: Text("Clear Contents"),
+              ),              
+            ],
           ),
+
         ],
       ),
     );
